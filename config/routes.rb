ec7119ed2fe7576f8d23618163f493467f80bce3
khoald-1241 <lam.dang.khoa@sun-asterisk.com>
2022-03-24 21:00:43 +0700

    Task 1: Add theme and create home page | Fixed 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    root to: "pages#home"
+    get "pages/home"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
