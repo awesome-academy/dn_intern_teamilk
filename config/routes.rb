a8e402bce1334fa9ab9bafac6f1535a8a59eda45
khoald-1241 <lam.dang.khoa@sun-asterisk.com>
2022-03-28 19:56:53 +0700

    task2_show_all_products

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root to: "pages#home"
     get "pages/home"
+    get "product/index"
+    get "login", to: "sessions#new"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
