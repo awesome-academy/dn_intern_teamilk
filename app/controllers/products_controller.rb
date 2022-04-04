559fc79fbd0e4f7fcf37aae2c1808190d7a7a945
truongha-1240 <hoang.anh.truong@sun-asterisk.com>
2022-04-04 13:39:24 +0700

    [User] Handle logic add product to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,14 +8,19 @@ class ProductsController < ApplicationController
 
   def show
     @product_details = @product.product_details.sortSizeAsc
+    return if @product_details.present?
+
+    flash[:danger] = t "product.out_of_stock"
+    redirect_to products_path
   end
 
   private
+
   def find_product
     @product = Product.find_by id: params[:id]
     return if @product
 
-    flash[:danger] = t "product_not_found"
+    flash[:danger] = t "product.product_not_found"
     redirect_to root_path
   end
 end
