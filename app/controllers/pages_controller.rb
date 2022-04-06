3762d8276878b5519d2ffbe34cf61696bf67f2c6
khoald-1241 <lam.dang.khoa@sun-asterisk.com>
2022-04-06 18:32:55 +0700

    Create Admin home page and fix imgae in cart page | fix 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,8 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+    return unless User.find_by(id: session[:user_id])&.admin?
+
+    flash[:success] = t "index.hello_admin"
+    redirect_to admin_path
+  end
 end
--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -6,4 +6,8 @@ module CartsHelper
   def caculator_price_in_cart price, quantity
     price * quantity
   end
+
+  def load_image_path_product product_id
+    "img/img_product/" + Product.find_by(id: product_id).image
+  end
 end
